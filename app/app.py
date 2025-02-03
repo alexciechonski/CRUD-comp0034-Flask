ebed9abcfd4293a20dafb7cd4e9c2d5387e34646
centralc2115 <zceeaci@ucl.ac.uk>
2025-02-03 14:32:13 +0000

    custom html

--- app/app.py
+++ app/app.py
@@ -1,2 +1,15 @@
 import pandas as pd
-from dash import html, dcc
+from dash import Dash, html, dcc
+
+
+app = Dash(__name__)
+
+with open("index2.html", "r", encoding="utf-8") as file:
+    app.index_string = file.read()
+
+app.layout = [
+    html.Div(children='My First App with Data and a Graph')
+]
+
+if __name__ == '__main__':
+    app.run(debug=True)
