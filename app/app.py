137b720046d75f6c11f167af23ada3b4ad002339
centralc2115 <zceeaci@ucl.ac.uk>
2025-02-04 14:40:16 +0000

    basic layout

--- app/app.py
+++ app/app.py
@@ -1,15 +1,74 @@
 import pandas as pd
 from dash import Dash, html, dcc
+import dash
 
 
 app = Dash(__name__)
 
-with open("index2.html", "r", encoding="utf-8") as file:
-    app.index_string = file.read()
-
 app.layout = [
-    html.Div(children='My First App with Data and a Graph')
+    html.Div(
+        className='container',
+        children=[
+            html.Div(
+                id="dataset-page",
+                className="section",
+                children=[
+                    html.H1("DATASET"),
+                    dcc.Graph()
+                ]
+            ),
+
+            html.Div(
+                id='time-series-page',
+                className="section",
+                children=[
+                    html.H1("TIME SERIES"),
+                    dcc.Graph()
+                ]
+            ),
+
+            html.Div(
+                id='restriction-distribution-page',
+                className="section",
+                children=[
+                    html.H1("RESTRICTION DISTRIBUTION"),
+                    dcc.Graph(),
+                    html.Div(
+                        className='user-input',
+                        children=[
+                            dcc.Slider(
+                                0, 20, 5,
+                                value=10,
+                                id='my-slider'
+                            ),
+                        ]
+                    ),
+                    html.P("or"),
+                    html.Div(
+                        className='text-input',
+                        children=[
+                            dcc.DatePickerSingle(
+                                id='date-input',
+                                date=None,
+                                placeholder='Date'
+                            )
+                        ]
+                    )
+                ]
+            ),
+
+            html.Div(
+                id='timeline-page',
+                className="section",
+                children=[
+                    html.H1("TIMELINE"),
+                    dcc.Graph()
+                ]
+            )
+        ]
+    )
 ]
+
 
 if __name__ == '__main__':
     app.run(debug=True)
