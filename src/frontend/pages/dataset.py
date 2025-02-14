6d0fc7c8c0cba9902337c5d196360a731b2985d0
centralc2115 <zceeaci@ucl.ac.uk>
2025-02-14 23:26:00 +0000

    remoed inputs from add

--- src/frontend/pages/dataset.py
+++ src/frontend/pages/dataset.py
@@ -160,6 +160,10 @@ layout = [
 
         Output('delete-input', 'options'),
 
+        #delete input
+        Output('table-name-input', 'value'),
+        Output('columns-input', 'value')
+
     ],
     [
         Input('select_db', 'value'), # select db
@@ -177,12 +181,12 @@ def update_erd_chart(select_db, clickData, back_btn, create_btn, delete_btn, col
 
     # go back
     if id == 'back-btn':
-        return dgms.erd(1), dict(display='none'), 0, None, 'covid.db', no_update
+        return dgms.erd(1), dict(display='none'), 0, None, 'covid.db', no_update, "", ""
 
     # show table
     elif id == 'erd-chart': 
         table_name = clickData['points'][0].get('text')
-        return dgms.get_table(select_db, table_name), dict(), no_update, no_update, no_update, show_tables(select_db)
+        return dgms.get_table(select_db, table_name), dict(), no_update, no_update, no_update, show_tables(select_db), "", ""
     
     # create table
     elif id == 'submit-create-button':
@@ -191,17 +195,17 @@ def update_erd_chart(select_db, clickData, back_btn, create_btn, delete_btn, col
         print("graph id", graph_id)
         print(new_table)
         crud.add_table(new_table, json.loads(cols), graph_id)
-        return dgms.erd(name_to_id[select_db]), no_update, 0, no_update, no_update, show_tables(select_db),
+        return dgms.erd(name_to_id[select_db]), no_update, 0, no_update, no_update, show_tables(select_db), "", ""
 
     # delete table
     elif id == 'submit-delete-button':
         crud = CRUD(select_db)
         crud.remove_table(select_db, delete_input)
         print(show_tables(select_db))
-        return dgms.erd(name_to_id[select_db]), no_update, 0, no_update, no_update, show_tables(select_db)
+        return dgms.erd(name_to_id[select_db]), no_update, 0, no_update, no_update, show_tables(select_db), "", ""
 
         
-    return dgms.erd(name_to_id[select_db]), dict(display='none'), 0, no_update, no_update, show_tables(select_db)
+    return dgms.erd(name_to_id[select_db]), dict(display='none'), 0, no_update, no_update, show_tables(select_db), "", ""
 
 # @callback(
 #     Output('select_db', 'options'),
