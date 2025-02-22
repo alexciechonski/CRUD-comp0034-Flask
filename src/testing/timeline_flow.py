13b9f1aa5695e56c4f600abeb1173d242710c490
centralc2115 <zceeaci@ucl.ac.uk>
2025-02-23 00:43:25 +0100

    optimized timeline

--- src/testing/timeline_flow.py
+++ src/testing/timeline_flow.py
@@ -1,37 +1,35 @@
-from playwright.sync_api import sync_playwright
-import time
+from playwright.sync_api import sync_playwright, Page
 import pytest
 
-def test_workflow():
-    res = {}
-
-    def click_timeline(node_name):
-        node = page.get_by_text(node_name)
-        box = node.bounding_box()
-        x, y = box['x'] + box['width']/2, box['y'] + box['height']/2 + 10
-        with page.expect_navigation():
-            page.mouse.click(x, y)
-        page.wait_for_load_state("networkidle")
-        res_url = page.evaluate("document.location.href")
-        page.wait_for_load_state("networkidle")
-        res[node_name] = res_url
-        page.goto("http://127.0.0.1:8050/timeline")
-        page.wait_for_load_state("networkidle")
-
+@pytest.fixture(scope="function")
+def browser():
+    """Setup and teardown Playwright browser instance."""
     with sync_playwright() as pw:
         browser = pw.chromium.launch(headless=False)
         page = browser.new_page()
-        page.goto("http://127.0.0.1:8050/timeline")
-        page.wait_for_load_state("networkidle")
+        yield page 
+        browser.close()
 
-        click_timeline("Lockdown 2")
+def click_timeline(page: Page, node_name):
+    node = page.get_by_text(node_name)
+    box = node.bounding_box()
+    x, y = box['x'] + box['width']/2, box['y'] + box['height']/2 + 10
+    with page.expect_navigation():
+        page.mouse.click(x, y)
+    page.wait_for_load_state("networkidle")
+    res_url = page.evaluate("document.location.href")
+    page.wait_for_load_state("networkidle")
+    page.goto("http://127.0.0.1:8050/timeline")
+    page.wait_for_load_state("networkidle")
+    return res_url
 
-        click_timeline("Plan B")
-
-        click_timeline("Stay Alert")
-
-    assert res == {
-        'Lockdown 2': 'https://www.gov.uk/government/publications/step-2-covid-19-restrictions-posters-12-april-2021',
-        'Plan B': 'https://www.gov.uk/government/speeches/pm-statement-at-coronavirus-press-conference-29-march-2021',
-        'Stay Alert': 'https://www.gov.uk/government/news/schools-colleges-and-early-years-settings-to-close'
-        }, "Incorrect Urls"
+@pytest.mark.parametrize("node_name, expected_url", [
+    ("Lockdown 2", "https://www.gov.uk/government/publications/step-2-covid-19-restrictions-posters-12-april-2021"),
+    ("Plan B", "https://www.gov.uk/government/speeches/pm-statement-at-coronavirus-press-conference-29-march-2021"),
+    ("Stay Alert", "https://www.gov.uk/government/news/schools-colleges-and-early-years-settings-to-close"),
+])
+def test_timeline_navigation(browser, node_name, expected_url):
+    """Tests timeline event navigation for different scenarios."""
+    browser.goto("http://127.0.0.1:8050/timeline")
+    result_url = click_timeline(browser, node_name)
+    assert result_url == expected_url, f"URL mismatch for {node_name}: Expected {expected_url}, got {result_url}"
