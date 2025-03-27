ecdc413268f06d31d7825fd08cc500e7cdefb90c
centralc2115 <zceeaci@ucl.ac.uk>
2025-03-27 19:06:48 +0000

    will work on backend val first

--- src/backend/log/log_manager.py
+++ src/backend/log/log_manager.py
@@ -1,8 +1,15 @@
 import json
 from src.config import LOG_PATH
+from collections import deque
 
 class LogManager:
     def __init__(self) -> None:
         with open(LOG_PATH, 'r') as f:
-            self.log = json.load(f)
+            log_data = json.load(f)
+        self.log = deque(log_data)
         self.length = len(self.log)
+
+    def update_length(self):
+        if self.length > 10:
+            self.log.popleft()
+    
