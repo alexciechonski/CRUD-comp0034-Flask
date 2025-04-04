fe5fadfd1e15322a9fb2f3137162bd1642735647
centralc2115 <zceeaci@ucl.ac.uk>
2025-04-04 18:19:04 +0200

    app tests work

--- src/testing/tests/unit/test_app.py
+++ src/testing/tests/unit/test_app.py
@@ -1,11 +1,6 @@
 import pytest
 from src.frontend.app import app
 
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
-
 def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
