be9e768859af5b0a88223e37818f3db6ce65e9a6
centralc2115 <zceeaci@ucl.ac.uk>
2025-04-04 14:13:41 +0200

    removed unused endpoints

--- src/testing/tests/unit/test_app.py
+++ src/testing/tests/unit/test_app.py
@@ -11,11 +11,6 @@ def test_index(client):
     assert response.status_code == 200
     assert b'COVID-19 Insights' in response.data
 
-def test_fetch_databases(client):
-    response = client.get('/api/databases')
-    assert response.status_code == 200
-    assert isinstance(response.json, list)
-
 def test_dataset(client):
     response = client.get('/dataset')
     assert response.status_code == 200
@@ -29,20 +24,6 @@ def test_time_series_get(client):
 def test_timeline(client):
     response = client.get('/timeline')
     assert response.status_code == 200
-
-def test_get_restrictions(client):
-    response = client.get('/api/restrictions')
-    assert response.status_code == 200
-    assert isinstance(response.json, list)
-
-def test_get_tables(client):
-    response = client.get('/api/tables/covid.db')
-    assert response.status_code == 200
-    assert isinstance(response.json, list)
-
-def test_analyze_data_invalid(client):
-    response = client.get('/api/analyze')
-    assert response.status_code == 400
 
 def test_table_crud(client):
     response = client.get('/crud-view')
