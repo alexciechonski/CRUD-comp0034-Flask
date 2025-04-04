df0154e07f46ad0c92a9708c51959039637ce860
centralc2115 <zceeaci@ucl.ac.uk>
2025-04-04 16:31:06 +0200

    utils tests fixed

--- src/testing/tests/unit/test_utils.py
+++ src/testing/tests/unit/test_utils.py
@@ -26,10 +26,18 @@ def test_get_table_info(table, db_path):
     exp = get_exp_data()
     assert get_table_info(table.lower(), db_path) == [tuple(row) for row in exp["table_info"][table]]
 
-def test_show_tables():
-    tables = show_tables("covid.db")
+@pytest.mark.parametrize(
+    "db_name",
+    [
+        ("covid.db"),
+        ("custom.db"),
+        ("deaths.db")
+    ]
+)
+def test_show_tables(db_name):
+    tables = show_tables(db_name)
     exp = get_exp_data()
-    assert tables == exp["db_table"]["covid.db"]
+    assert tables == exp["db_table"][db_name]
 
 def test_get_databases():
     dbs = get_databases()
@@ -47,11 +55,12 @@ def test_create_database():
     create_database("test.db")
     return "test.db" in get_databases()
 
-def test_table_not_empty():
-    query_db("CREATE TABLE test_table (col INTEGER)", get_db_path("test.db"))
-    assert not table_not_empty("test.db", "test_table")
-    query_db("INSERT INTO test_table (col) VALUES (1)", get_db_path("test.db"))
-    assert table_not_empty("test.db", "test_table")
+def test_table_not_empty(test_db_engine):
+    with test_db_engine.connect() as conn:
+        conn.execute("CREATE TABLE test_table (col INTEGER)")
+        assert not table_not_empty("test.db", "test_table")
+        conn.execute("INSERT INTO test_table (col) VALUES (1)")
+        assert table_not_empty("test.db", "test_table")
 
 def test_delete_database():
     delete_database("test.db")
@@ -68,6 +77,3 @@ def test_delete_database():
 def test_get_primary_keys(table, db_name):
     exp = get_exp_data()
     assert get_primary_keys(table.lower(), db_name) == exp["primary_keys"][db_name][table]
-
-if __name__ == "__main__":
-    print(get_databases())
